945678cd118e70d86c0261d2775be96969093e5f
Harshit Vaibhav <harshitvaibhav1610@gmail.com>
2024-08-11 19:28:26 +0530

    first commit

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "vav@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accountId = 2// not allowed
+ accountEmail = "hvh.com"
+  accountPassword = "12795"
+accountCity = "patna"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+/*
---
+++ 02_dataTypes.js
@@ -0,0 +1,19 @@
+    "use strict";//treat all js code as newer version
+// alert("hello") we are using nodejs,not browser
+
+let name ="vaibhav"
+let age = 18
+let isLoggedIn = false
+
+// number =>2 to power 53
+// bigint  big numbers
+// string =>""
+// boolean => true/flase
+// null => standalone value
+// undefined =>
+// symbol =>unique
+
+
+// object 
+
+console.log(typeof undefined);
