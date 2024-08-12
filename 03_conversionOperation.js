deaaaa42e1e8585b5f16394af364e570ccdfdcfd
Harshit Vaibhav <harshitvaibhav1610@gmail.com>
2024-08-12 21:18:27 +0530

    config conversion done

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -1,24 +1,61 @@
-let score = "33ab"
-console.log(typeof score);
-console.log(typeof (score));
+// let score = "33ab"
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-//"33" =>33
-//"33abc" =>NaN
-//true => 1;false =>0
+// //"33" =>33
+// //"33abc" =>NaN
+// //true => 1;false =>0
 
-let isLoggedIn =1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let isLoggedIn =1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
-//true => 1;false =>0
-//"" =>false
-//"vaibhav" =>true
+// //true => 1;false =>0
+// //"" =>false
+// //"vaibhav" =>true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+/******************operation*********** */
+
+let value =3
+let negValue =-value
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 ="hello"
+// let str2 = " vaibhav"
+
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(true);
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
