f2e31f692c58ccae76e9d000cf31be6e7b87cca1
Harshit Vaibhav <harshitvaibhav1610@gmail.com>
2024-08-13 01:49:41 +0530

    datatype summary

---
+++ dataTypes-summary.js
@@ -0,0 +1,38 @@
+// primitive data types
+// 7types
+// -String,Number,Boolean,null,undefined,Symbol,BigInt
+
+// java script is dynamically type language
+
+//  const score = 100
+//  const scoreValue =100.3
+  
+//  const isLoggedIn = false
+ const outsideTemp = null
+//  let userEmail;
+
+//  const id = Symbol('123')
+//  const anotherId = Symbol('123')
+//  console.log(id===anotherId);
+ 
+// const bigNumber = 34565435n
+
+// Reference(Non Primitive)
+// Array,Objects,Function
+
+// const heroes =["shaktiman","naagraj","doga"]
+
+
+
+// let myObj ={
+//     name:"vaibhav",
+//     age:24,
+// }
+
+
+// const myFunction = function(){
+//     console.log("hello");
+    
+// }
+
+console.log(typeof outsideTemp);
